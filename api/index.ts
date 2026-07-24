b0bed5d3f24c425858446a4b474ba54a168f01a2
rgcouk <rgcouk@users.noreply.github.com>
2026-07-24 21:06:56 +0100

    Fix Prisma output path requirement for Prisma 7

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../src/generated/prisma/client.ts';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 import dotenv from 'dotenv';
